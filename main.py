b663b8f4d34a4b01a74fa042f8b695bb362c611c
nnneeerrraaajjj <fizzintan@gmail.com>
2023-07-08 10:47:28 +0530

    add transcribe audio

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 from fastapi import FastAPI, UploadFile, Form, File
 from sound_features import (estimate_pitch_yin, measure_speech_clarity_mfcc, estimate_tonality_hps,
                             measure_energy_rms, detect_silence)
+from audio_to_text import transcribe_it
 
 app = FastAPI()
 
@@ -21,18 +23,22 @@ async def process_audio(audioFile: UploadFile = File(...), cal: str = Form(...))
     sp = estimate_pitch_yin(audioFile.filename)
     se = measure_energy_rms(audioFile.filename)
     ss = detect_silence(audioFile.filename)
+    transcript = await transcribe_it(audioFile.filename)
 
     sc_judger = "Good" if sc >= 0.5 else "Bad"
     st_judger = "Good" if st is not None else "Bad"
     sp_judger = "Good" if sp > 0 else "Bad"
     se_judger = "Good" if se >= 0.1 else "Bad"
 
+    os.remove(audioFile.filename)
+
     return {
         "message": "Audio processed",
-        "cal": cal,
+        "question": cal,
         "speech_clarity": {"value": sc, "judger": sc_judger},
         "tonality": {"value": st, "judger": st_judger},
         "speech_pitch": {"value": sp, "judger": sp_judger},
         "speech_energy": {"value": se, "judger": se_judger},
-        "speech silence": ss
+        "speech silence": ss,
+        "transcript": transcript
     }
